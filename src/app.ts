aa751f3c1c9ec7367e37b8f82770eeed6f0b1241
MacJ13 <jacdim02@gmail.com>
2025-06-28 18:16:27 +0200

    feat(server): add basic Express setup
    - import express, Request, Response, NextFunction
    - configure JSON/body parse middleware
    - define GET "/" riute returning "Hello World"
    - add handler 'page not exist' and error middleware
    - start listening on port 8000

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("app.ts");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err = new Error("Page not exist!");
+
+  next(err);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    console.log(err);
+  }
+  res.send(err.message);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
