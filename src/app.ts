a6f520eb1010b37c315ce0bd0a4510585263696c
MacJ13 <jacdim02@gmail.com>
2025-06-29 14:46:54 +0200

    feat(view): add EJS templating engine
    - intall 'ejs' dependency
    - set 'views engine'  to 'ejs' in app.ts
    - configure 'views' directory for templates
    - render 'index' view in handler '/' route
    - create index.ejs in views/pages directory

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,24 @@ import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 
 import { PORT } from "./config/env.config";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  // res.send("Hello World");
+  res.render("pages/index", { message: "Hello World" });
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
