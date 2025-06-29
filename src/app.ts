64c3f1121ab2e2193dd9346335cffbaf12887dcc
MacJ13 <jacdim02@gmail.com>
2025-06-29 20:22:35 +0200

    feat(style): integrate Tailwind CSS
    - install @tailwindcss/cli, @tailwindcss/postcss, autoprefixer, postcss,
      postcss-cli and tailwind dependencies
    - create tailiwind.config.js and postcss.config.js
    - create public/css folder with css file
    - update index template view with html structure
    - add public folder as app static files in app.ts
    - add css scripts in package.json

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/public", express.static(path.join(__dirname, "../public")));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
