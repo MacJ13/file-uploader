39f15773ec09b64ee03496074f5f0bfcb67827cf
MacJ13 <jacdim02@gmail.com>
2025-08-26 00:18:38 +0200

    refactor(error): update rendering error view
    - add user from request and pass to render view
    - add message and statusCode properties to render view
    - display new heading with message and status property in error view

--- src/middlewares/handleError.ts
+++ src/middlewares/handleError.ts
@@ -2,15 +2,21 @@ import { ErrorHandlerType } from "../types/handlers";
 import CustomError from "../utils/errors/CustomError";
 
 const handleError: ErrorHandlerType = (err, req, res, next) => {
+  const user = req.user;
+
   if (err instanceof CustomError) {
     const errorStatusCode = err.getStatusCode();
+    const errorMessage = err.message;
 
-    if (errorStatusCode == 404) {
-      res.statusCode = errorStatusCode;
+    res.statusCode = errorStatusCode;
 
-      res.render("pages/404");
-      return;
-    }
+    res.render("pages/404", {
+      user,
+      title: "error page",
+      message: errorMessage,
+      statusCode: errorStatusCode,
+    });
+    return;
   }
 };
 
